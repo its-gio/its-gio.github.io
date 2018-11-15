569b7501799091323d9f92b122f7f697156379d5
its-gio <me@its-g.io>
2018-11-14 16:59:54 -0800

    Changing port to 3001

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-app.set('port', (process.env.PORT || 80))
-app.use(express.static(__dirname + '/public'))
+app.set("port", process.env.PORT || 3001);
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-})
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"));
+});
