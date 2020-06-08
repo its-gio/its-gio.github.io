3d29d46d4a082c774be57924f58c5b438cdfffa9
Giovani Aguirre <me@its-g.io>
2020-06-08 14:56:41 -0700

    Added triangle overlap effect and image of owner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Banner from "./components/Banner";
+import Meet from "./components/Meet";
 
 function App() {
   return (
     <>
       <Banner />
+      <Meet />
     </>
   );
 }
---
+++ src/components/Meet.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Gio from "../img/Gio.jpg";
+
+function Meet() {
+  return (
+    <div className="meet">
+      <div>
+        <h2>Testing</h2>
+        <p>P testing!</p>
+      </div>
+      <div>
+        <img src={Gio} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Meet;
