380614abd6867ec8b9f9c04da77ddcc0724fe89e
Giovani Aguirre <me@its-g.io>
2020-06-08 15:12:03 -0700

    Created Mixin for Title and Tagline. Styles remain mainly the same thoughout components.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className="banner">
       <div className="banner--inner">
-        <h1 className="banner--inner__intro">Hey, I'm Gio!</h1>
+        <h1 className="banner--inner__title">Hey, I'm Gio!</h1>
         <p className="banner--inner__tagline">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Id sunt
           ratione eligendi nulla libero beatae laborum ipsa illum adipisci
