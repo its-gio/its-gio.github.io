229be0fa9920c7efe965980b49c93fe59d35be9d
Giovani Aguirre <me@its-g.io>
2020-06-08 16:06:18 -0700

    Fixed TNT Mixin and Banner style combo. Craeted new Mixin for overlap and added styles for Meet component.

--- src/components/Meet.jsx
+++ src/components/Meet.jsx
@@ -4,12 +4,24 @@ import Gio from "../img/Gio.jpg";
 function Meet() {
   return (
     <div className="meet">
-      <div>
-        <h2>Testing</h2>
-        <p>P testing!</p>
-      </div>
-      <div>
-        <img src={Gio} alt="" />
+      <div className="meet-inner">
+        <div className="meet-inner--content">
+          <h2 className="meet-inner--content__title">
+            I'm Gio and I like Muffins
+          </h2>
+          <p className="meet-inner--content__tagline">
+            Ok muffins aren't that great. With their weird shape. You can't even
+            eat it in one bite. It's so odd. Hot dogs on the other hand are also
+            not that great.
+          </p>
+        </div>
+        <div className="meet-inner--img-container">
+          <img
+            className="meet-inner--img-container__img"
+            src={Gio}
+            alt="Owner of this wonderful site."
+          />
+        </div>
       </div>
     </div>
   );
