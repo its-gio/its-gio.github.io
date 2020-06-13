ca3e291ea67720f3937df2e8dd2580744a3cf655
Giovani Aguirre <me@its-g.io>
2020-06-13 15:05:27 -0700

    Skillset section started. Title and tagline onlong with background added.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss";
 import Banner from "./components/Banner";
 import Meet from "./components/Meet";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <>
       <Banner />
       <Meet />
+      <Skills />
     </>
   );
 }
